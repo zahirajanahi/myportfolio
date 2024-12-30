7987903c9bdd0bd1b455b4231a5e96bc287857f8
zahirajanahi <jude75418@gmail.com>
2024-12-30 16:29:24 +0100

    push

--- src/constant/images.jsx
+++ src/constant/images.jsx
@@ -1,13 +1,8 @@
 export default {
-    html:require("../assets/images/images-removebg-preview.png"),
-    react:require('../assets/images/react-native.png'),
-    js:require('../assets/images/javascript.png'),
-    lrvl:require('../assets/images/raf_360x360_075_t_fafafa_ca443f4786-removebg-preview.png'),
-    git:require('../assets/images/logomark-white_2x-removebg-preview.png'),
-    cofee:require('../assets/images/coffee.png'),
-    sg:require('../assets/images/socialGeek.png'),
-    fsh:require('../assets/images/fash.png'),
-    delicious:require('../assets/images/violence.png'),
-    phone:require('../assets/images/Cdfghjapture-removebg-preview.png'),
-    phone2:require('../assets/images/phone-removebg-preview.png'),
-}
+    hero:require("../assets/images/hero.jpg"),
+    me:require("../assets/images/me.jpeg"),
+    sg:require("../assets/images/projet.png"),
+    hh:require("../assets/images/hh.png"),
+    ff:require("../assets/images/fff.png"),
+
+} 
